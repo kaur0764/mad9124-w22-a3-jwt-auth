aa0f8b45cdc6f3cd8b95fc6f0dc2f2a02d04e3d3
JasreetK <90123505+JasreetK@users.noreply.github.com>
2022-03-21 19:08:36 +0530

    Refactor the Mongoose model for User to encrypt password before storing

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
+import bcrypt from "bcrypt";
 import mongoose from "mongoose";
+
+const saltRounds = 14;
 
 const schema = new mongoose.Schema({
   firstName: { type: String, required: true, maxLength: 64 },
@@ -7,6 +10,14 @@ const schema = new mongoose.Schema({
   password: { type: String, required: true, maxLength: 70 },
   isAdmin: { type: Boolean, required: true, default: false },
 });
+
+schema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, saltRounds);
+  next();
+});
+
 const Model = mongoose.model("User", schema);
 
 export default Model;
