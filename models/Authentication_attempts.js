82038136b71d15c52fa34782f6f80b2206edb637
JasreetK <90123505+JasreetK@users.noreply.github.com>
2022-03-23 00:56:42 +0530

    Add the Mongoose model for Authentication_attempts

---
+++ models/Authentication_attempts.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema({
+  username: { type: String, required: true, maxLength: 64 },
+  ipAddress: { type: String, required: true, maxLength: 64 },
+  didSucceed: { type: Boolean, required: true },
+  createdAt: { type: String, required: true },
+});
+const Model = mongoose.model("Authentication_attempts", schema);
+
+export default Model;
