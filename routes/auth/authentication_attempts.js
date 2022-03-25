0e107c926e535362615f5a46d45ea215434dbfb8
JasreetK <90123505+JasreetK@users.noreply.github.com>
2022-03-26 00:59:01 +0530

    Format the response data object from authentication_attempts collection

--- routes/auth/authentication_attempts.js
+++ routes/auth/authentication_attempts.js
@@ -6,7 +6,25 @@ const router = express.Router();
 
 router.get("/", authenticate, async (req, res) => {
   const authentication_attempts = await Authentication_attempts.find();
-  res.json({ data: authentication_attempts });
+  res.json({
+    data: authentication_attempts.map((authentication_attempt) =>
+      formatResponseData(
+        "authentication_attempts",
+        authentication_attempt.toObject()
+      )
+    ),
+  });
 });
+
+/**
+ * Format the response data object according to JSON:API v1.0
+ * @param {string} type The resource collection name, e.g. ' authentication_attempts'
+ * @param {Object} resource An instance object from that collection
+ * @returns
+ */
+function formatResponseData(type, resource) {
+  const { _id, ...attributes } = resource;
+  return { type, id: _id, attributes };
+}
 
 export default router;
